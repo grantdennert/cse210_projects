93ab554f4589f79be7f772ae1aa55192d4ec5fe1
Grant Dennert <grantdennert@hotmail.com>
2026-09-19 22:38:32 -0600

    completed Prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,10 +6,8 @@ class Program
     {
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-        Console.WriteLine();
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
-        Console.WriteLine();
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
