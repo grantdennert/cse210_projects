c7ed602351f3e0762fe9539cf2f17f68d91603f0
Grant Dennert <grantdennert@hotmail.com>
2026-09-23 14:41:07 -0600

    completed assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string response = "yes";
+        int guess;
+        int guessCounter;
+        do
+        {
+            int magicNumber = randomGenerator.Next(1,100);
+            guessCounter = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCounter ++;
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Total guesses: {guessCounter}");
+                }
+            } while (guess != magicNumber);
+            Console.WriteLine("Do you want to continue?");
+            response = Console.ReadLine();
+        } while (response == "yes");
+        Console.WriteLine("Goodbye!");
     }
 }
